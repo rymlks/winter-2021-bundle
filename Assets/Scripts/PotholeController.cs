09615e0a142df8074cd5ae7d3bcaf04c835ef60a
Sapphon <sapphon@gmail.com>
2020-12-24 23:59:44 -0500

    PotholeController now adds potholes to the game when rounds end

---
+++ Assets/Scripts/PotholeController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotholeController : MonoBehaviour
+{
+    public int minimumNewHolesPerRound;
+    public int maximumNewHolesPerRound;
+    public float maximumPotholeAngerPerRound;
+    public GameObject potholePrefab;
+    private GameObject potholeParent;
+
+    private float WIDTH_OF_PLAY_AREA = 20f;
+    private float HEIGHT_OF_PLAY_AREA = 9.5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (potholePrefab == null)
+        {
+            potholePrefab = Resources.Load<GameObject>("Prefabs/Pothole");
+        }
+
+        if (potholeParent == null)
+        {
+            potholeParent = new GameObject("Potholes");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roundHasEndedThisFrame())
+        {
+            createNewPotholes();
+        }
+    }
+
+    private void createNewPotholes()
+    {
+        int newPotholesThisRound = new System.Random().Next(minimumNewHolesPerRound, maximumNewHolesPerRound + 1);    //Sysrand is exclusive of the maximum
+        for (int i = 0; i < newPotholesThisRound; i++)
+        {
+            GameObject created = GameObject.Instantiate(potholePrefab, generatePotholeLocation(), Quaternion.identity, potholeParent.transform);
+        }
+    }
+
+    private Vector2 generatePotholeLocation()
+    {
+        //integrate the real map when we have one
+        //for now, anyplace the pothole is fully visible to the camera is a valid place for it to appear
+        float x = Random.value * WIDTH_OF_PLAY_AREA;
+        float z = Random.value * HEIGHT_OF_PLAY_AREA;
+        return new Vector2((-WIDTH_OF_PLAY_AREA / 2f) + x,(-HEIGHT_OF_PLAY_AREA / 2f) + z);
+    }
+
+    private bool roundHasEndedThisFrame()
+    {
+        //integrate the real round system when we have one
+        //for now, rounds can be faked as ending by the player pressing 'R'
+        return Input.GetKeyUp(KeyCode.R);
+    }
+}
