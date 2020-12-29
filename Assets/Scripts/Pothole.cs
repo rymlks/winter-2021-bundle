7ccb50c256358cc205d58e1caf4ebb1356a057d6
Sapphon <sapphon@gmail.com>
2020-12-29 18:40:43 -0500

    Potholes are now patchable (although clicking them doens't do it yet) and produce no anger when patched

--- Assets/Scripts/Pothole.cs
+++ Assets/Scripts/Pothole.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class Pothole : MonoBehaviour
 {
     private float _angerPerRound;
+    private bool _isPatched;
+    private float _timeLastPatched;
+    public Sprite potholeSprite;
+    public Sprite patchedPotholeSprite;
     
     void Start()
     {
         this._angerPerRound = Random.value * FindObjectOfType<BalanceParameters>().maximumPotholeAngerPerRound;
+        this._isPatched = false;
+        this._timeLastPatched = -1f;
+        RenderNormal();
     }
+
+    
 
     public float getAngerCausedPerRound()
     {
-        return this._angerPerRound;
+        return this._isPatched ? 0 : this._angerPerRound;
     }
 
+    public void Patch()
+    {
+        this._isPatched = true;
+        this._timeLastPatched = Time.time;
+        RenderPatch();
+    }
+
+    private void RenderNormal()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = potholeSprite;
+    }
+    
+    private void RenderPatch()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = patchedPotholeSprite;
+    }
 }
