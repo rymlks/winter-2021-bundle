4763d76ebdb2d932bf1a7a72a866bc04bb068b41
Roland J Jarvis <Rjarvis87@gmail.com>
2021-01-30 18:51:15 -0500

    Music Toggle added to UI.prefab; TitleScreen>Music.obj is made into a prefab and added to Utils folder; 2 scripts added [FadeTheButton, MutesTheMusic] to Scripts folder;

---
+++ Assets/Scripts/FadeTheButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTheButton : MonoBehaviour
+{
+    private Image Image;
+    private Color color;
+    private bool fadingTheImage;
+    private void Awake() {
+        Image = this.GetComponent<Image>();
+        color = Image.color;
+    }
+
+    public void FadeTheImage() {
+        fadingTheImage = !fadingTheImage;
+
+        if (fadingTheImage) {
+            color.a = 0.5f; //if fadingTheImage == true then set the opacity to half
+        } else {
+            color.a = 1f; //else the opacity is full
+        }
+
+        Image.color = color; //set the color on the Image to the color modified in this script.
+    }
+}
