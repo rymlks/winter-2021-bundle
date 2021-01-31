5eec766eb5c35c8eb3d898f93383a1479ec50ecd
Sapphon <sapphon@gmail.com>
2021-01-31 05:22:51 -0500

    Tutorial appears in progressive steps
    And in its own UI window

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.SmartFormat;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
 
     [TextArea(15,20)]
     public string tutorialText;
+    
+    [TextArea(15,20)]
+    public string tutorialIntroText;
+    
+    [TextArea(15,20)]
+    public string tutorialHowToWinText;
+    
+    [TextArea(15,20)]
+    public string tutorialAngerText;
+    
+    [TextArea(15,20)]
+    public string tutorialBudgetText;
+    
+    [TextArea(15,20)]
+    public string tutorialLaborAndOutroText;
+
+    private string[] _tutorialTextsOrdered;
+    protected int currentTextIndex = 0;
+
+    protected bool shouldShowTutorial = true;
+    protected GameObject tutorialMenuObject;
+    protected GameManager manager;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        this.tutorialMenuObject = GameObject.Find("TutorialMenu");
     }
 
     // Update is called once per frame
@@ -18,22 +45,65 @@ public class TutorialController : MonoBehaviour
     {
         
     }
-    
+
+    void OrderTutorialTexts()
+    {
+        _tutorialTextsOrdered = new string[5];
+        _tutorialTextsOrdered[0] = tutorialIntroText;
+        _tutorialTextsOrdered[1] = tutorialHowToWinText;
+        _tutorialTextsOrdered[2] = tutorialAngerText;
+        _tutorialTextsOrdered[3] = tutorialBudgetText;
+        _tutorialTextsOrdered[4] = tutorialLaborAndOutroText;
+        currentTextIndex = 0;
+    }
+
     public void NotifyGameBeginning(GameManager manager)
     {
+        SetManager(manager);
+        OrderTutorialTexts();
         if (ShouldShowTutorial())
         {
-            manager.contextMenu.Open(Smart.Format(tutorialText, manager), GetRoadmapCenter());
+            tutorialMenuObject.SetActive(true);
+            UpdateText();
         }
     }
 
-    private static Vector3 GetRoadmapCenter()
+    private void UpdateText()
     {
-        return new Vector3((Road.MaxX + Road.MinX) * 0.5f, (Road.MaxY + Road.MinY) * 0.5f, 0.0f);
+        GetMessageText().text =
+            Smart.Format(_tutorialTextsOrdered[currentTextIndex], this.manager);
     }
 
+    private void SetManager(GameManager manager)
+    {
+        this.manager = manager;
+    }
+
+    private Text GetMessageText()
+    {
+        return tutorialMenuObject.transform.Find("BlackBorder/OrangeBorder/Body/Message").gameObject.GetComponent<Text>();
+    }
+
+    public void AdvanceTutorial()
+    {
+        this.currentTextIndex = (this.currentTextIndex + 1) % _tutorialTextsOrdered.Length;
+        UpdateText();
+        
+    }
+    
     private bool ShouldShowTutorial()
     {
-        return true;
+        return shouldShowTutorial;
+    }
+
+    public void NeverShowAgain()
+    {
+        tutorialMenuObject.SetActive(false);
+        this.shouldShowTutorial = false;
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialMenuObject.SetActive(false);
     }
 }
