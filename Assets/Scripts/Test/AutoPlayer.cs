fb80fb82c3e0858e9cafe792750d43e6b94c6096
Sapphon <sapphon@gmail.com>
2021-02-07 01:30:47 -0500

    Autoplayer can now end rounds

--- Assets/Scripts/Test/AutoPlayer.cs
+++ Assets/Scripts/Test/AutoPlayer.cs
@@ -8,22 +8,21 @@ public class AutoPlayer : MonoBehaviour, GameManagerObserver
 {
 
     private AutoplayerPotholeStrategy potholeStrategy;
-    public 
-    // Start is called before the first frame update
-    void Start()
+    public bool autoEndRounds = true;
+    public void Start()
     {
         potholeStrategy = new ThrowNRollEverythingPotholeStrategy();
         FindObjectOfType<GameManager>().RegisterObserver(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void NotifyRoundBeginning(GameManager manager)
     {
         potholeStrategy.execute(manager);
+        if(autoEndRounds){endRound(manager);}
+    }
+
+    protected void endRound(GameManager manager)
+    {
+        manager.NextRound();
     }
 }
